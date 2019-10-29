199550cdb13494945e1d43cc94134c41a6d03a3a
lange <lange@infoboard.biz>
2019-10-29 16:19:09 +0100

    add sql injection

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,61 @@ var router = express.Router();
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "192.168.99.100",
-  user: "root",
-  password: "password123",
-  database: "my-db"
+    host: "192.168.99.100",
+    user: "root",
+    password: "password123",
+    database: "my-db"
 });
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+// Tabelle löschen
+// con.query("DROP TABLE Safe;", function (err, rows, fields) {
+//     if (err) throw err;
+//     console.log('The solution is: ', rows);
+// });
+
+// Tabelle neu erstellen
+// con.query("CREATE TABLE Safe (personalKey int NOT NULL, title varchar(100), secret varchar(100), PRIMARY KEY(personalKey));", function (err, rows, fields) {
+//     if (err) throw err;
+//     console.log('The solution is: ', rows);
+// });
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var user = req.query.user;
+    res.render('index', {title: 'Hallo, speichere deine geheime Nachricht mit deinem eigenen Schlüssel'});
+});
 
+/**
+ * Speichere eine geheime Nachricht
+ */
+router.get('/save', function (req, res, next) {
+    var personalKey = req.query.personalKey;
+    var title = req.query.title;
+    var secret = req.query.secret;
 
-    con.connect(function (err) {
+    con.query("INSERT INTO Safe (personalKey, title, secret) VALUES (" + personalKey + ", '" + title + "', '" + secret + "') ", function (err, rows, fields) {
         if (err) throw err;
-        console.log("Connected!");
     });
 
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: 'Geheime Nachricht gespeichert'});
+});
+
+/**
+ * lese die Geheime Nachricht aus dem Tresor aus
+ */
+router.get('/read', function (req, res, next) {
+    var personalKey = req.query.key;
+
+    //todo prevent SQL-Injections
+
+    con.query("SELECT * FROM Safe WHERE personalKey=" + personalKey + ";", function (err, rows, fields) {
+        if (err) throw err;
+        res.render('index', {title: 'Geheime Nachricht gefunden', data: rows});
+    });
 });
 
 module.exports = router;
