86c6f583ef28832cef3a89297192e7d2a64b4244
lange <lange@infoboard.biz>
2019-10-29 16:42:05 +0100

    add index '/'

--- routes/index.js
+++ routes/index.js
@@ -27,29 +27,32 @@ con.connect(function (err) {
 //
 // }
 
-// try {
-//     // Tabelle neu erstellen
-//     con.query("CREATE TABLE Safe (personalKey int NOT NULL, title varchar(100), secret varchar(100), PRIMARY KEY(personalKey));", function (err, rows, fields) {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-//         console.log('The solution is: ', rows);
-//     });
-// } catch (e) {
-//
-// }
+try {
+    // Tabelle neu erstellen
+    con.query("CREATE TABLE Safe (personalKey int NOT NULL, title varchar(100), secret varchar(100), PRIMARY KEY(personalKey));", function (err, rows, fields) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('The solution is: ', rows);
+    });
+} catch (e) {
 
+}
+
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Hallo, speichere deine geheime Nachricht mit deinem eigenen Schlüssel'});
+});
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/secretMessage', function (req, res, next) {
     res.render('index', {title: 'Hallo, speichere deine geheime Nachricht mit deinem eigenen Schlüssel'});
 });
 
 /**
  * Speichere eine geheime Nachricht
  */
-router.get('/save', function (req, res, next) {
+router.get('/secretMessage/save', function (req, res, next) {
     var personalKey = req.query.personalKey;
     var title = req.query.title;
     var secret = req.query.secret;
@@ -64,7 +67,7 @@ router.get('/save', function (req, res, next) {
 /**
  * lese die Geheime Nachricht aus dem Tresor aus
  */
-router.get('/read', function (req, res, next) {
+router.get('/secretMessage/read', function (req, res, next) {
     var personalKey = req.query.key;
 
     //todo prevent SQL-Injections
