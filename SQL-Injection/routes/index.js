aa9b15907f9d873a507362ca7ef2f3023e06a331
Jonas Sannewald <jonas.sannewald@gmail.com>
2019-11-04 00:52:40 +0100

    SQL-Injection Protection hinzugefügt

--- SQL-Injection/routes/index.js
+++ SQL-Injection/routes/index.js
@@ -57,13 +57,44 @@ router.get('/secretMessage/save', function (req, res, next) {
     var personalKey = req.query.personalKey;
     var title = req.query.title;
     var secret = req.query.secret;
+    
 
+    // Unsichere Version
     con.query("INSERT INTO Safe (personalKey, title, secret) VALUES (" + personalKey + ", '" + title + "', '" + secret + "');", function (err, rows, fields) {
         if (err){
             console.log('Something went wrong during insert');
             console.log(err)
         }
     });
+
+
+    // Prepared Statement
+    /*con.query("INSERT INTO Safe (personalKey, title, secret) VALUES (?, ?, ?);",
+        [personalKey, title, secret],
+        function (err, rows, fields) {
+        if (err){
+            console.log('Something went wrong during insert');
+            console.log(err)
+        }
+    });*/
+
+
+    // WHITELIST NICHT UMSETZBAR -> OR TRUE
+
+
+    // Escaping
+    /*console.log("\n\nEingabe: " + personalKey + ", " + title + ", " + secret + " -> Escaped: " + con.escape(personalKey) + ", " + con.escape(title) + ", " + con.escape(secret) + "\n\n");
+    con.query("INSERT INTO Safe (personalKey, title, secret) VALUES ("+con.escape(personalKey)+", "+con.escape(title)+", "+con.escape(secret)+");", function (err, rows, fields) {
+        if (err){
+            console.log('Something went wrong during insert');
+            console.log(err)
+        }
+    });*/
+
+
+    // STORED PROCEDURES
+
+
     console.log(`NEW ROW ${personalKey} | ${title} | ${secret}`);
     res.render('index', {title: 'Geheime Nachricht gespeichert'});
 });
@@ -74,13 +105,49 @@ router.get('/secretMessage/save', function (req, res, next) {
 router.get('/secretMessage/read', function (req, res, next) {
     var personalKey = req.query.key;
 
-    //todo prevent SQL-Injections
-
+    
+    // Unsichere Version
     con.query("SELECT * FROM Safe WHERE personalKey=" + personalKey + ";", function (err, rows, fields) {
         if (err) console.log('Something went wrong during select');
         console.log(`SHOW ${personalKey}`);
         res.render('index', {title: 'Geheime Nachricht gefunden', data: rows});
     });
+
+
+    // Prepared Statement
+    /*con.query("SELECT * FROM Safe WHERE personalKey=?;",
+        [personalKey],
+        function (err, rows, fields) {
+        if (err) console.log('Something went wrong during select');
+        console.log(`SHOW ${personalKey}`);
+        res.render('index', {title: 'Geheime Nachricht gefunden', data: rows});
+    });*/
+
+
+    // Whitelist
+    /*if(isNaN(personalKey)) {
+        res.send('<div style="color:#FFFFFF; background-color: red; margin-top: 100px; text-align: center; line-height: 100px; height: 100px; width: 100%; font-size: 25px;">Keine Zahl eingegeben!</div>');
+    } else {
+        con.query("SELECT * FROM Safe WHERE personalKey=" + personalKey + ";", function (err, rows, fields) {
+            if (err) console.log('Something went wrong during select');
+            console.log(`SHOW ${personalKey}`);
+            res.render('index', {title: 'Geheime Nachricht gefunden', data: rows});
+        });
+    }*/
+
+
+    // Escaping
+    /*console.log("\n\nEingabe: " + personalKey + " -> Escaped: " + con.escape(personalKey) + "\n\n");
+    con.query("SELECT * FROM Safe WHERE personalKey=" + con.escape(personalKey) + ";", function (err, rows, fields) {
+        if (err) console.log('Something went wrong during select');
+        console.log(`SHOW ${personalKey}`);
+        res.render('index', {title: 'Geheime Nachricht gefunden', data: rows});
+    });*/
+
+
+    // STORED PROCEDURES
+    
+    
 });
 
 module.exports = router;
